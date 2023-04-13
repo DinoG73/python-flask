9a2e9c9db385e272053e5695eda342a3b1b1f453
DinoG73 <53279025+DinoG73@users.noreply.github.com>
2023-04-13 11:36:50 -0300

    Start python101

---
+++ ejercicios/eje1-print.py
@@ -0,0 +1,32 @@
+#1) Escribe un programa muestre por pantalla “Hello World”
+print('hola bruno')
+print ('Vamos para adelante')
+
+#2) Escribe un programa que escriba en la pantalla el resultado de sumar 3 + 5.
+print(3+5)
+#3) Escribe un programa que pida el nombre del usuario y escriba un texto que diga “Hola nombreUsuario”
+#nombre = input("Ingrese su nombre: ")
+#print('Hola', nombre)
+
+
+#4) Escribe un programa que pida un número, pida otro número y escriba el resultado de sumar estos dos números.
+#num1 = float(input("ingrese un numero: "))
+#num2 = float(input("ingrese un numero: "))
+#print(num1 +num2)
+
+#5) Escribe un programa que pida dos números y escriba en la pantalla cual es el mayor.
+#num1 = float(input("ingrese un numero: "))
+#num2 = float(input("ingrese un numero: "))
+'''
+if num1 > num2:
+    print(num1)
+else:
+    print(num2)    
+
+'''
+#6) Escribe un programa que pida 3 números y escriba en la pantalla el mayor de los tres.
+
+#7) Escribe un programa que pida un número y diga si es divisible por 2
+#8) Escribe un programa que pida un número y nos diga si es divisible por 2, 3, 5 o 7 (sólo hay que comprobar si lo es por uno de los cuatro)
+#9) Añadir al ejercicio anterior que nos diga por cuál de los cuatro es divisible (hay que decir todos por los que es divisible)
+
